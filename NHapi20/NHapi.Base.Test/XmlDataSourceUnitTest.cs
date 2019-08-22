c59752b78f501be9e0177ecc9f135f36572a3272
Neil NACARIO <neil.nacario@gmail.com>
2019-08-23 01:22:43 +0800

    Added xml datasource

---
+++ NHapi20/NHapi.Base.Test/XmlDataSourceUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NHapi.Base.DataSource;
+using NHapi.Base.SourceGeneration;
+
+namespace NHapi.Base.Test
+{
+	 [TestClass]
+	 public class XmlDataSourceUnitTest : GeneratorUnitTestBase
+	 {
+			[TestMethod]
+			public void TestMethod1()
+			{
+				 // Arrange
+				 var baseFolder = GetBaseFolder();
+				 var version = GetVersion();
+				 var source = new NHapi.Base.DataSource.Xml.MessageSource();
+				 source.FileName = "Mocks\\reverse-nHapi.xml";
+				 var messageTargetFolder = GetTargetFolder("Message");
+				 var groupTargetFolder = GetTargetFolder("Group");
+
+				 DataSourceFactory.SetMessageSource(source);
+
+				 // Act
+				 MessageGenerator.makeAll(baseFolder, version);
+
+				 // Assert
+			}
+	 }
+}
---
+++ NHapi20/NHapi.Base/DataSource/Xml/MessageSource.cs
@@ -0,0 +1,116 @@
+﻿using NHapi.Base.SourceGeneration;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace NHapi.Base.DataSource.Xml
+{
+	 public class MessageSource : IMessageSource, ISegmentSource, IDataTypeSource, IEventMappingSource
+	 {
+			private bool _initialized;
+			private HL7Versions _hl7Versions;
+
+			public string FileName { get; set; }
+
+			public MessageSource()
+			{
+				 _initialized = false;
+				 _hl7Versions = null;
+			}
+
+			#region IMessageSource implementation
+			public SegmentDef[] GetSegments(string message, string version)
+			{
+				 LoadXmlFile();
+				 var hl7Grammar = _hl7Versions.HL7Version.Where(v => v.Version.Equals(version)).SingleOrDefault()
+																		.Grammars.Where(g => g.Message.Equals(message)).SingleOrDefault();
+				 ArrayList segments = new ArrayList();
+				 if (null != hl7Grammar)
+				 {
+						foreach (var segment in hl7Grammar.Segments)
+						{
+							 segments.Add(new SegmentDef(segment.Name, segment.Group, segment.Required, segment.Repeating, segment.Description));
+						}
+				 }
+				 SegmentDef[] ret = new SegmentDef[segments.Count];
+				 Array.Copy(segments.ToArray(), ret, segments.Count);
+				 return ret;
+			}
+			public void GetMessages(string version, out ArrayList messages, out ArrayList chapters)
+			{
+				 LoadXmlFile();
+				 messages = new ArrayList();
+				 chapters = new ArrayList();
+				 var hl7Version = _hl7Versions.HL7Version.Where(v => v.Version.Equals(version)).SingleOrDefault();
+				 if (null != hl7Version)
+				 {
+						foreach (var grammar in hl7Version.Grammars)
+						{
+							 messages.Add(grammar.Message);
+							 chapters.Add(grammar.Chapter);
+						}
+				 }
+			}
+			#endregion
+
+			#region ISegmentSource implementation
+			public ArrayList GetSegments(string version)
+			{
+				 LoadXmlFile();
+				 throw new NotImplementedException();
+			}
+
+			public void GetSegmentDefinition(string name, string version, out ArrayList elements, out string segDesc)
+			{
+				 LoadXmlFile();
+				 throw new NotImplementedException();
+			}
+			#endregion
+
+			#region IDataTypeSource implementation
+			public ArrayList GetTypes(string version)
+			{
+				 LoadXmlFile();
+				 throw new NotImplementedException();
+			}
+			public void GetComponentDataType(string dataType, string version, out ArrayList dataTypes, out ArrayList descriptions, out ArrayList tables, out string description)
+			{
+				 LoadXmlFile();
+				 throw new NotImplementedException();
+			}
+			#endregion
+
+			#region IEventMappingSource implementation
+			public void GetMessageMapping(string version, out ArrayList messageTypes, out ArrayList events, out ArrayList messageStructures)
+			{
+				 LoadXmlFile();
+				 throw new NotImplementedException();
+			}
+			#endregion
+
+			#region Utilities
+			private void LoadXmlFile()
+			{
+				 if (!_initialized)
+				 {
+						if (!string.IsNullOrEmpty(FileName))
+						{
+							 XmlSerializer serializer = new XmlSerializer(typeof(HL7Versions));
+							 using (TextReader reader = new StreamReader(FileName))
+							 {
+									_hl7Versions = (HL7Versions)serializer.Deserialize(reader);
+							 }
+							 _initialized = true;
+						}
+						else
+						{
+							 throw new Exception("Filename was not supplied.");
+						}
+				 }
+			}
+			#endregion
+	 }
+}
