5f98a7e6bb4d06f47e69bce5c0ec9db7c3c0752a
Neil NACARIO <neil.nacario@gmail.com>
2019-08-23 12:35:01 +0800

    Added IEventMappingSource and IDataTypeSource implementation.

--- NHapi20/NHapi.Base.Test/XmlDataSourceUnitTest.cs
+++ NHapi20/NHapi.Base.Test/XmlDataSourceUnitTest.cs
@@ -45,5 +45,64 @@ namespace NHapi.Base.Test
 
 				 // Assert
 			}
+
+			[TestMethod]
+			public void makeAll_DataTypesCreated_DefinitionGiven()
+			{
+				 // Arrange
+				 var baseFolder = GetBaseFolder();
+				 var version = GetVersion();
+				 var source = new NHapi.Base.DataSource.Xml.MessageSource();
+				 source.FileName = "Mocks\\reverse-nHapi.xml";
+				 var messageTargetFolder = GetTargetFolder("Message");
+				 var groupTargetFolder = GetTargetFolder("Group");
+
+				 DataSourceFactory.SetDataTypeSource(source);
+
+				 // Act
+				 DataTypeGenerator.makeAll(baseFolder, version);
+
+				 // Assert
+			}
+
+			[TestMethod]
+			public void makeAll_EventMappingCreated_DefinitionGiven()
+			{
+				 // Arrange
+				 var baseFolder = GetBaseFolder();
+				 var version = GetVersion();
+				 var source = new NHapi.Base.DataSource.Xml.MessageSource();
+				 source.FileName = "Mocks\\reverse-nHapi.xml";
+				 var messageTargetFolder = GetTargetFolder("Message");
+				 var groupTargetFolder = GetTargetFolder("Group");
+
+				 DataSourceFactory.SetEventMappingSource(source);
+
+				 // Act
+				 EventMappingGenerator.makeAll(baseFolder, version);
+
+				 // Assert
+			}
+			[TestMethod]
+			public void makeAll_AllCreated_DefinitionGiven()
+			{
+				 // Arrange
+				 var baseFolder = GetBaseFolder();
+				 var version = GetVersion();
+				 var source = new NHapi.Base.DataSource.Xml.MessageSource();
+				 source.FileName = "Mocks\\reverse-nHapi.xml";
+				 var messageTargetFolder = GetTargetFolder("Message");
+				 var groupTargetFolder = GetTargetFolder("Group");
+
+				 DataSourceFactory.SetMessageSource(source);
+				 DataSourceFactory.SetSegmentSource(source);
+				 DataSourceFactory.SetDataTypeSource(source);
+				 DataSourceFactory.SetEventMappingSource(source);
+
+				 // Act
+				 SourceGenerator.makeAll(baseFolder, version);
+
+				 // Assert
+			}
 	 }
 }
--- NHapi20/NHapi.Base/DataSource/Xml/MessageSource.cs
+++ NHapi20/NHapi.Base/DataSource/Xml/MessageSource.cs
@@ -78,7 +78,6 @@ namespace NHapi.Base.DataSource.Xml
 				 elements = new ArrayList();
 				 segDesc = string.Empty;
 				 var hl7Grammar = _hl7Versions.HL7Version.Where(v => v.Version.Equals(version)).SingleOrDefault();
-				 ArrayList segments = new ArrayList();
 				 if (null != hl7Grammar)
 				 {
 						var segment = hl7Grammar.Segments.Where(s => s.Name.Equals(name)).SingleOrDefault();
@@ -90,9 +89,9 @@ namespace NHapi.Base.DataSource.Xml
 							 {
 									field = field.Order,
 									type = field.Type,
-									opt = (!field.Required).ToString(),
+									opt = field.Required ? "R" : "O",
 									repetitions = field.Repeations,
-									length = (int)field.Length,
+									length = field.Length,
 									table = string.IsNullOrEmpty(field.Table) ? 0 : Convert.ToInt32(field.Table),
 									desc = field.Description
 							 };
@@ -106,12 +105,40 @@ namespace NHapi.Base.DataSource.Xml
 			public ArrayList GetTypes(string version)
 			{
 				 LoadXmlFile();
-				 throw new NotImplementedException();
+				 var hl7Grammar = _hl7Versions.HL7Version.Where(v => v.Version.Equals(version)).SingleOrDefault();
+				 ArrayList dataTypes = new ArrayList();
+				 if (null != hl7Grammar)
+				 {
+						foreach (var type in hl7Grammar.Datatypes)
+						{
+							 dataTypes.Add(type.Name);
+						}
+				 }
+				 return dataTypes;
 			}
 			public void GetComponentDataType(string dataType, string version, out ArrayList dataTypes, out ArrayList descriptions, out ArrayList tables, out string description)
 			{
 				 LoadXmlFile();
-				 throw new NotImplementedException();
+				 dataTypes = new ArrayList();
+				 descriptions = new ArrayList();
+				 tables = new ArrayList();
+				 description = string.Empty;
+				 var hl7Grammar = _hl7Versions.HL7Version.Where(v => v.Version.Equals(version)).SingleOrDefault();
+				 if (null != hl7Grammar)
+				 {
+						var type = hl7Grammar.Datatypes.Where(s => s.Name.Equals(dataType)).SingleOrDefault();
+						description = type.Description;
+						if (null != type.Composites)
+						{
+							 foreach (var composite in type.Composites)
+							 {
+									dataTypes.Add(composite.Name);
+									descriptions.Add(composite.Description);
+									tables.Add(composite.Table);
+							 }
+						}
+
+				 }
 			}
 			#endregion
 
@@ -119,7 +146,19 @@ namespace NHapi.Base.DataSource.Xml
 			public void GetMessageMapping(string version, out ArrayList messageTypes, out ArrayList events, out ArrayList messageStructures)
 			{
 				 LoadXmlFile();
-				 throw new NotImplementedException();
+				 messageTypes = new ArrayList();
+				 events = new ArrayList();
+				 messageStructures = new ArrayList();
+				 var hl7Grammar = _hl7Versions.HL7Version.Where(v => v.Version.Equals(version)).SingleOrDefault();
+				 if (null != hl7Grammar)
+				 {
+						foreach (var mapEntry in hl7Grammar.Mapping)
+						{
+							 messageTypes.Add(mapEntry.Type);
+							 events.Add(mapEntry.Event);
+							 messageStructures.Add(mapEntry.Structure);
+						}
+				 }
 			}
 			#endregion
 
